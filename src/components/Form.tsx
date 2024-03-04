9102c2eeeac4413c9f1ba5143be41d7be8ec1b04
Claudio Meira <claudiomeirabs@gmail.com>
2024-03-04 11:42:06 -0300

    Requisito 02 feito

---
+++ src/components/Form.tsx
@@ -0,0 +1,22 @@
+function Form() {
+  return (
+    <>
+      <label htmlFor="service-name">Nome do serviço</label>
+      <input type="text" name="service-name" id="service-name" />
+
+      <label htmlFor="login">Login</label>
+      <input type="text" name="login" id="login" />
+
+      <label htmlFor="password">Senha</label>
+      <input type="password" name="password" id="password" />
+
+      <label htmlFor="URL">URL</label>
+      <input type="text" name="URL" id="URL" />
+
+      <button type="submit">Cadastrar</button>
+      <button type="button">Cancelar</button>
+    </>
+  );
+}
+
+export default Form;
