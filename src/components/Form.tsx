c4778385d953c5776200251f4df4e9a3d7a75ba4
Claudio Meira <claudiomeirabs@gmail.com>
2024-03-05 12:08:38 -0300

    Requisito 04 feito

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,73 @@
+import { useState } from 'react';
+
 type FormProps = {
   handleClick: ()=> void
 };
 
 function Form({ handleClick }:FormProps) {
+  const [data, setData] = useState({
+    serviceName: '',
+    login: '',
+    password: '',
+  });
+
+  const validateForm = () => {
+    const hasSpecial = /[!@#$%^&*(),.?":{}|<>]+/;
+    const testSpecial = hasSpecial.test(data.password);
+    const hasAny = /\d/;
+    const testAny = hasAny.test(data.password);
+    const hasLetter = /[a-zA-Z]/;
+    const testLetter = hasLetter.test(data.password);
+
+    return data.password.length > 8
+      && data.password.length < 16
+      && testSpecial && testAny
+      && testLetter && !!data.login && !!data.serviceName;
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
   return (
     <>
       <label htmlFor="service-name">Nome do serviço</label>
-      <input type="text" name="service-name" id="service-name" />
+      <input
+        type="text"
+        value={ data.serviceName }
+        name="serviceName"
+        id="service-name"
+        onChange={ handleChange }
+      />
 
       <label htmlFor="login">Login</label>
-      <input type="text" name="login" id="login" />
+      <input
+        type="text"
+        value={ data.login }
+        name="login"
+        id="login"
+        onChange={ handleChange }
+      />
 
       <label htmlFor="password">Senha</label>
-      <input type="password" name="password" id="password" />
+      <input
+        type="password"
+        value={ data.password }
+        name="password"
+        id="password"
+        onChange={ handleChange }
+      />
 
       <label htmlFor="URL">URL</label>
       <input type="text" name="URL" id="URL" />
 
-      <button type="submit">Cadastrar</button>
+      <button type="submit" disabled={ !validateForm() }>Cadastrar</button>
       <button type="button" onClick={ handleClick }>Cancelar</button>
     </>
   );
